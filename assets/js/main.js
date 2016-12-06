fd3f8dbba8bf56a723d36a7a281b8203d60f4499
Vipul Bansal <vipul.bansal@Vipuls-MacBook-Air.local>
2016-12-06 12:06:43 +0530

    GA CD8

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,10 +22,10 @@ $(document).ready(function() {
     // $('#gender').on('click', function() { var dimensionValue = $(this).val(); });
     $(function() {
         var $vvForm = $('#vv-form');
-        ga('set', {
-            'dimension1': $('#gender :selected').val(),
-            'metric1': 1
-        });
+        // ga('set', {
+        //     'dimension1': $('#gender :selected').val(),
+        //     'metric1': 1
+        // });
         $vvForm.submit(function(e) {
             e.preventDefault();
             $.ajax({
@@ -39,6 +39,9 @@ $(document).ready(function() {
                 success: function(data) {
                     console.log(data);
                     $('input[type="submit"]').val('Thank You').prop('disabled', true);
+                    console.log($('#gender :selected').val());
+                    ga('set', 'dimension1', $('#gender :selected').val());
+                    console.log($('#gender :selected').val());
                     // ga('set', 'dimension1', dimensionValue);
                     // ga('send', 'pageview', '/work/');
                     // console.log(dimensionValue);
